428a4b2a4c56d5c4fa961b207ed48738a4b23404
Kevin Medeiros <kevin.medeiros.k@gmail.com>
2018-10-11 01:43:09 -0300

    test Nonexistent user

--- main_test.go
+++ main_test.go
@@ -3,13 +3,17 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
 
 var a App
 
+// Test base connection and setup database
 func TestMain(m *testing.M) {
 	a = App{}
 	a.Initialize("root", "test123", "test_api")
@@ -42,3 +46,46 @@ CREATE TABLE IF NOT EXISTS users
 	age INT NOT NULL
 )
 `
+
+// Test empty tables
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/users", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+// TestNonexistentUser
+func TestGetNonExistentUser(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/user/45", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "User not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'User not found'. Got '%s'", m["error"])
+	}
+}
