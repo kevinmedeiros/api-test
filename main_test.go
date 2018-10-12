7ad19572c322969ad9daa408076980f5bb941ff3
Kevin Medeiros <kevin.medeiros.k@gmail.com>
2018-10-12 09:39:18 -0300

    .

--- main_test.go
+++ main_test.go
@@ -3,17 +3,19 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 )
 
 var a App
 
-// Test base connection and setup database
 func TestMain(m *testing.M) {
 	a = App{}
 	a.Initialize("root", "test123", "test_api")
@@ -41,13 +43,11 @@ func clearTable() {
 const tableCreationQuery = `
 CREATE TABLE IF NOT EXISTS users
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	age INT NOT NULL
-)
-`
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+)`
 
-// Test empty tables
 func TestEmptyTable(t *testing.T) {
 	clearTable()
 
@@ -74,7 +74,6 @@ func checkResponseCode(t *testing.T, expected, actual int) {
 	}
 }
 
-// TestNonexistentUser
 func TestGetNonExistentUser(t *testing.T) {
 	clearTable()
 
@@ -88,4 +87,103 @@ func TestGetNonExistentUser(t *testing.T) {
 	if m["error"] != "User not found" {
 		t.Errorf("Expected the 'error' key of the response to be set to 'User not found'. Got '%s'", m["error"])
 	}
+}
+
+func TestCreateUser(t *testing.T) {
+	clearTable()
+
+	payload := []byte(`{"name":"test user","age":30}`)
+
+	req, _ := http.NewRequest("POST", "/user", bytes.NewBuffer(payload))
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusCreated, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != "test user" {
+		t.Errorf("Expected user name to be 'test user'. Got '%v'", m["name"])
+	}
+
+	if m["age"] != 30.0 {
+		t.Errorf("Expected user age to be '30'. Got '%v'", m["age"])
+	}
+
+	// the id is compared to 1.0 because JSON unmarshaling converts numbers to
+	// floats, when the target is a map[string]interface{}
+	if m["id"] != 1.0 {
+		t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
+	}
+}
+
+func addUsers(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		statement := fmt.Sprintf("INSERT INTO users(name, age) VALUES('%s', %d)", ("User " + strconv.Itoa(i+1)), ((i + 1) * 10))
+		a.DB.Exec(statement)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	clearTable()
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func TestUpdateUser(t *testing.T) {
+	clearTable()
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+	var originalUser map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalUser)
+
+	payload := []byte(`{"name":"test user - updated name","age":21}`)
+
+	req, _ = http.NewRequest("PUT", "/user/1", bytes.NewBuffer(payload))
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["id"] != originalUser["id"] {
+		t.Errorf("Expected the id to remain the same (%v). Got %v", originalUser["id"], m["id"])
+	}
+
+	if m["name"] == originalUser["name"] {
+		t.Errorf("Expected the name to change from '%v' to '%v'. Got '%v'", originalUser["name"], m["name"], m["name"])
+	}
+
+	if m["age"] == originalUser["age"] {
+		t.Errorf("Expected the age to change from '%v' to '%v'. Got '%v'", originalUser["age"], m["age"], m["age"])
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	clearTable()
+	addUsers(1)
+
+	req, _ := http.NewRequest("GET", "/user/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/user/1", nil)
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/user/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
 }
